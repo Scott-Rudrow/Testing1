691562de83ded2afeb8ae13308a00d45eb2b70f9
Scott Rudrow <SERudrow@student.fullsail.edu>
2025-03-02 18:15:27 -0700

    Mouse Positioning and Cartesian Coordinate Correction

--- MainForm.h
+++ MainForm.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string>
 #include <array>
+#include <Math.h>
 #define SDL_MAIN_HANDLED
 // Remove the semicolon at the end of the macro definition
 
@@ -112,56 +113,68 @@ private:
 
 	}
 #pragma endregion
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		Close();
-	}
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		System::Void GameMain(int argc, char* argv[]); {
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	Close();
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	System::Void GameMain(int argc, char* argv[]); {
 		// Add game main logic 
-			SDL_Window* window;
-			SDL_Renderer* renderer;
-			MessageBox::Show("Game Start.");
-			window = SDL_CreateWindow("Alien War", 1920, 720, 0);
+		SDL_Window* window;
+		SDL_Renderer* renderer;
+		MessageBox::Show("Game Start.");
+		window = SDL_CreateWindow("Alien War", 1920, 720, 0);
 
-			renderer = SDL_CreateRenderer(window, "Alien War");
+		renderer = SDL_CreateRenderer(window, "Alien War");
 
-			if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-				SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-				return;
+		if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+			SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+			return;
+		}
+
+		if (window == NULL) {
+			SDL_Log("Could not create window: %s\n", SDL_GetError());
+			return;
+		}
+
+		SDL_Log("SDL3 INITIATED");
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1);
+
+		bool running = true;
+		while (running) {
+			SDL_Event event;
+			if (SDL_EVENT_MOUSE_MOTION * event.type) {
+				float x, y;
+				SDL_GetMouseState(&x, &y);
+				//std::cout << x << ":" << y << std::endl;
+				SDL_Log("Mouse position: (%f, %f)", x, abs(y - 719));
 			}
-			
-			if (window == NULL) {
-				SDL_Log("Could not create window: %s\n", SDL_GetError());
-				return;
-			}
 
-			SDL_Log("SDL3 INITIATED");
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1);
-			
-			bool running = true;
-			while (running) {
-				SDL_Event event;
-				while (SDL_PollEvent(&event) >0) {
+			while (SDL_PollEvent(&event)); {
+				if (SDL_EVENT_KEY_DOWN == event.type) {
 					switch (event.type) {
 					case SDL_EVENT_QUIT:
+					SDL_EVENT_WINDOW_CLOSE_REQUESTED:
 						SDL_Log("SDL3 Quit Event");
 						running = false;
 						break;
+
+
+
+
+						//SDL_Log("Game shut down");
+						//SDL_DestroyRenderer(renderer);
+						//SDL_DestroyWindow(window);
+
+
+						return;
 					}
 				}
-
 			}
-			//SDL_Log("Game shut down");
-			//SDL_DestroyRenderer(renderer);
-			//SDL_DestroyWindow(window);
+		}
 
-
-		return;
-	}
-
-	/*private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		/*private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		GameMain();
-	}
+
 		Form^ newForm = gcnew Form();
 		newForm->Show();
 		this->Hide();
@@ -169,4 +182,5 @@ private:
 
 
 	}
+};
 };
